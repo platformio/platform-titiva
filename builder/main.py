c6f4e497a19ce848bf0a4876f9c7143a5d060a46
Ivan Kravets <me@ikravets.com>
2016-05-27 19:16:21 +0300

    Initial commit

---
+++ builder/main.py
@@ -0,0 +1,155 @@
+# Copyright 2014-present Ivan Kravets <me@ikravets.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+    Builder for Texas Instruments
+    Tiva C Series ARM Cortex-M4 microcontrollers.
+"""
+
+from os.path import join
+
+from SCons.Script import (COMMAND_LINE_TARGETS, AlwaysBuild, Builder, Default,
+                          DefaultEnvironment)
+
+env = DefaultEnvironment()
+
+env.Replace(
+    AR="arm-none-eabi-ar",
+    AS="arm-none-eabi-as",
+    CC="arm-none-eabi-gcc",
+    CXX="arm-none-eabi-g++",
+    OBJCOPY="arm-none-eabi-objcopy",
+    RANLIB="arm-none-eabi-ranlib",
+    SIZETOOL="arm-none-eabi-size",
+
+    ARFLAGS=["rcs"],
+
+    ASFLAGS=["-x", "assembler-with-cpp"],
+
+    CCFLAGS=[
+        "-g",   # include debugging info (so errors include line numbers)
+        "-Os",  # optimize for size
+        "-ffunction-sections",  # place each function in its own section
+        "-fdata-sections",
+        "-Wall",
+        "-mthumb",
+        "-nostdlib"
+    ],
+
+    CXXFLAGS=[
+        "-fno-rtti",
+        "-fno-exceptions"
+    ],
+
+    CPPDEFINES=[
+        "F_CPU=$BOARD_F_CPU"
+    ],
+
+    LINKFLAGS=[
+        "-Os",
+        "-Wl,--gc-sections,--relax",
+        "-mthumb",
+        "-nostartfiles",
+        "-nostdlib"
+    ],
+
+    LIBS=["c", "gcc", "m"],
+
+    UPLOADER="lm4flash",
+    UPLOADCMD='$UPLOADER $SOURCES',
+
+    SIZEPRINTCMD='$SIZETOOL -B -d $SOURCES',
+
+    PROGNAME="firmware",
+    PROGSUFFIX=".elf"
+)
+
+if "BOARD" in env:
+    env.Append(
+        CCFLAGS=[
+            "-mcpu=%s" % env.BoardConfig().get("build.cpu")
+        ],
+        LINKFLAGS=[
+            "-mcpu=%s" % env.BoardConfig().get("build.cpu")
+        ]
+    )
+
+env.Append(
+    ASFLAGS=env.get("CCFLAGS", [])[:],
+
+    BUILDERS=dict(
+        ElfToBin=Builder(
+            action=" ".join([
+                "$OBJCOPY",
+                "-O",
+                "binary",
+                "$SOURCES",
+                "$TARGET"]),
+            suffix=".bin"
+        ),
+        ElfToHex=Builder(
+            action=" ".join([
+                "$OBJCOPY",
+                "-O",
+                "ihex",
+                "-R",
+                ".eeprom",
+                "$SOURCES",
+                "$TARGET"]),
+            suffix=".hex"
+        )
+    )
+)
+
+
+#
+# Target: Build executable and linkable firmware
+#
+
+target_elf = env.BuildProgram()
+
+#
+# Target: Build the .bin file
+#
+
+if "uploadlazy" in COMMAND_LINE_TARGETS:
+    target_firm = join("$BUILD_DIR", "firmware.bin")
+else:
+    target_firm = env.ElfToBin(join("$BUILD_DIR", "firmware"), target_elf)
+
+#
+# Target: Print binary size
+#
+
+target_size = env.Alias("size", target_elf, "$SIZEPRINTCMD")
+AlwaysBuild(target_size)
+
+#
+# Target: Upload firmware
+#
+
+upload = env.Alias(["upload", "uploadlazy"], target_firm, "$UPLOADCMD")
+AlwaysBuild(upload)
+
+#
+# Target: Unit Testing
+#
+
+AlwaysBuild(env.Alias("test", [target_firm, target_size]))
+
+#
+# Target: Define targets
+#
+
+Default([target_firm, target_size])
